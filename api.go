9837d88dce878a021fc1f1ac396b3e4b79172abb
Michael <michael.lindman@gmail.com>
2020-07-31 16:33:46 +0100

    data prarm for post now uses bytes buffer and removed hard-coded json headers
    Signed-off-by: Michael <michael.lindman@gmail.com>

--- api.go
+++ api.go
@@ -20,13 +20,11 @@ type Response struct {
 }
 
 // API sends RESTful API requests
-func API(method, url string, headers map[string]string, data []byte) (*Response, error) {
-	req, err := http.NewRequest(method, url, bytes.NewBuffer(data))
+func API(method, url string, headers map[string]string, data *bytes.Buffer) (*Response, error) {
+	req, err := http.NewRequest(method, url, data)
 	if err != nil {
 		return &Response{method, http.StatusInternalServerError, req.URL, nil, err}, err
 	}
-	req.Header.Set("Accept", "application/json")
-	req.Header.Set("Content-Type", "application/json")
 	for k, v := range headers {
 		req.Header.Set(k, v)
 	}
@@ -49,7 +47,7 @@ func API(method, url string, headers map[string]string, data []byte) (*Response,
 }
 
 // AsyncAPI send requests concurrently
-func AsyncAPI(method, url string, headers map[string]string, data []byte, ch chan<- *Response, wg *sync.WaitGroup) {
+func AsyncAPI(method, url string, headers map[string]string, data *bytes.Buffer, ch chan<- *Response, wg *sync.WaitGroup) {
 	defer wg.Done()
 	resp, _ := API(method, url, headers, data)
 	ch <- resp
